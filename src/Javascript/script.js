a84fc315bff7de9e30ec8b8c32d693bf17605da8
matbeds <1295100@sga.pucminas.br>
2026-05-20 20:01:56 -0300

    Update script.js

--- src/Javascript/script.js
+++ src/Javascript/script.js
@@ -23,3 +23,33 @@ function direcionarUsuario() {
         alert("Por favor, selecione uma opção: Adotar ou Doar.");
     }
 }
+
+function previewImages() {
+    const previewContainer = document.getElementById('preview-container');
+    const files = document.getElementById('petPhotos').files;
+    
+    // Limpa o container antes de mostrar novas fotos
+    previewContainer.innerHTML = '';
+
+    // Verifica se passou de 3 fotos
+    if (files.length > 3) {
+        alert("Por favor, selecione no máximo 3 fotos.");
+        document.getElementById('petPhotos').value = ''; // Reseta o input
+        return;
+    }
+
+    // Loop pelos arquivos selecionados
+    for (let i = 0; i < files.length; i++) {
+        const file = files[i];
+        const reader = new FileReader();
+
+        reader.onload = function(e) {
+            const img = document.createElement('img');
+            img.src = e.target.result;
+            img.classList.add('img-preview');
+            previewContainer.appendChild(img);
+        }
+
+        reader.readAsDataURL(file);
+    }
+}
